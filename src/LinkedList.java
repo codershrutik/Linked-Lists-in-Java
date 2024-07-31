b14fc97c1493a704353e892fb11a2f882be1d3e5
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-07-31 17:38:11 +0000

    printlist method added

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -18,5 +18,25 @@ public class LinkedList {
         tail=newNode;
         length=1;
     }
+
+    public void printList(int value){
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp=temp.next;
+        }
+    }
+
+    public void getHead(){
+        System.out.println("Head: "+head.value);
+    }
+
+    public void getTail(){
+        System.out.println("Tail: "+tail.value);
+    }
+
+    public void getLength(){
+        System.out.println("Length: "+length);
+    }
     
 }
