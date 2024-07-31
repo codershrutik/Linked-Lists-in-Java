64f6ee6912c73d738b08c0e3234238ee40baa64b
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-07-31 17:41:54 +0000

    append method added

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         
-        LinkedList myLinkedList = new LinkedList(4);
+        LinkedList myLinkedList = new LinkedList(1);
+
+        // myLinkedList.getHead();
+        // myLinkedList.getTail();
+        // myLinkedList.getLength();
+
+        myLinkedList.append(2);
 
         myLinkedList.printList(0);
 
-        myLinkedList.getHead();
-        myLinkedList.getTail();
-        myLinkedList.getLength();
     }
     
 }
