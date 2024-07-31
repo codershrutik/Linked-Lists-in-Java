cb060e53dbf97e18623115aff73096e73baf2811
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-07-31 18:15:16 +0000

    get method added

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         
-        LinkedList myLinkedList = new LinkedList(1);
+        LinkedList myLinkedList = new LinkedList(0);
 
         // myLinkedList.getHead();
         // myLinkedList.getTail();
         // myLinkedList.getLength();
 
+        myLinkedList.append(1);
         myLinkedList.append(2);
-        myLinkedList.prepend(0);
-        myLinkedList.printList();
+        myLinkedList.append(3);
 
-        System.out.println(myLinkedList.removeFirst());
+        System.out.println(myLinkedList.get(2));
+        // myLinkedList.prepend(0);
+        // myLinkedList.printList();
+
+        // System.out.println(myLinkedList.removeFirst());
+
 
         // System.out.println(myLinkedList.removeLast().value);
         // System.out.println(myLinkedList.removeLast().value);
