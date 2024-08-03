96577ae31d6907cb54eb12327c55b38c34577d9a
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-08-03 05:27:50 +0000

    DLL prepend method added

--- src/Main.java
+++ src/Main.java
@@ -31,11 +31,13 @@ public class Main {
         // myDLL.getTail();
         // myDLL.getLength();
         myDLL.append(2);
-        // myDLL.printList();
+        myDLL.prepend(0);
+        myDLL.printList();
 
-        System.out.println(myDLL.removeLast().value);
-        System.out.println(myDLL.removeLast().value);
-        System.out.println(myDLL.removeLast());
+
+        // System.out.println(myDLL.removeLast().value);
+        // System.out.println(myDLL.removeLast().value);
+        // System.out.println(myDLL.removeLast());
        
     }
     
